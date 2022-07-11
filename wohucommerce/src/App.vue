e8f0ed73ddedb865e78d9dcb1f7eb7f54e8b5091
Tony Obriku <tonyobriku@gmail.com>
2022-07-11 01:03:55 +0100

    first commit

---
+++ wohucommerce/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"
+          ><strong>Wohu</strong></router-link
+        >
+        <a href=""></a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style>
+</style>
